d547a50858f45daf4f764e1fd75df300a7b86799
betulakan <betulakan5972@gmail.com>
2022-04-24 23:12:02 +0300

    Tween Animation Project added

---
+++ app/src/main/java/com/example/tweenanimation/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.tweenanimation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var image:ImageView
+    private lateinit var button: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        image = findViewById(R.id.imageView)
+        button = findViewById(R.id.button)
+        val anim = AnimationUtils.loadAnimation(this, R.anim.my_animation)
+
+        button.setOnClickListener {
+            image.startAnimation(anim)
+        }
+
+    }
+}
